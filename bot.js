eb0871f77f8afe2b1b387209d52361e7a4d5bdad
Adam <adam.a.pacheco@gmail.com>
2022-05-10 20:21:04 -0400

    Added quotation to all keys for rpc api

--- bot.js
+++ bot.js
@@ -128,16 +128,16 @@ var n = 1;
 var len = 14;
 var ls = 'abcdefghijklmnopqrstuvwxyz 0123456789'
 var options = {
-		jsonrpc: '2.0',
-		method: 'generateStrings',
-		params: {
-			apiKey: process.env.RANDOM_API_KEY,
-			n: 1,
-			length: 14,
-			characters: `${ls}`,
-			replacement: true,
+		'jsonrpc': '2.0',
+		'method': 'generateStrings',
+		'params': {
+			'apiKey': process.env.RANDOM_API_KEY,
+			'n': 1,
+			'length': 14,
+			'characters': `${ls}`,
+			'replacement': true
 		},
-		id: 7294
+		'id': 7294
 	}
 	request.post('https://api.random.org/json-rpc/1/invoke',
 	{
